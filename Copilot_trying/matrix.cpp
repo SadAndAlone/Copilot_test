ab17f0f41d6eb93f1aa78deb2f094a840dadf9e5
TrudovMykhailo <156135082+SadAndAlone@users.noreply.github.com>
2024-12-09 14:24:35 +0100

    konstruktory i destruktory matrix.cpp

--- Copilot_trying/matrix.cpp
+++ Copilot_trying/matrix.cpp
@@ -7,3 +7,31 @@ void matrix::allocateMemory(int size) {
         data[i] = new int[size]();
     }
 }
+// --- Konstruktory i destruktor ---
+matrix::matrix() : n(0), data(nullptr) {}
+
+matrix::matrix(int n) : n(n) {
+    allocateMemory(n);
+}
+
+matrix::matrix(int n, int* t) : n(n) {
+    allocateMemory(n);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            data[i][j] = t[i * n + j];
+}
+
+matrix::matrix(const matrix& m) : n(m.n) {
+    allocateMemory(n);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            data[i][j] = m.data[i][j];
+}
+
+matrix::~matrix() {
+    if (data) {
+        for (int i = 0; i < n; ++i)
+            delete[] data[i];
+        delete[] data;
+    }
+}
