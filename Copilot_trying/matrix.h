0948d875f5d13c45daf953c6dfac54d64f743b43
TrudovMykhailo <156135082+SadAndAlone@users.noreply.github.com>
2024-12-09 14:21:22 +0100

    matrix.h

---
+++ Copilot_trying/matrix.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+class matrix {
+private:
+    int n;          // Rozmiar macierzy (n x n)
+    int** data;     // Wskaźnik na dynamicznie alokowaną macierz
+
+    void allocateMemory(int size);
