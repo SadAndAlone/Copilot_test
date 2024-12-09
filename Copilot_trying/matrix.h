ff86bf49eebe429f6f00dea6d97b51429e8bce03
TrudovMykhailo <156135082+SadAndAlone@users.noreply.github.com>
2024-12-09 14:22:01 +0100

    konstruktory i destruktory main.h

--- Copilot_trying/matrix.h
+++ Copilot_trying/matrix.h
@@ -14,3 +14,26 @@ private:
     int** data;     // Wskaźnik na dynamicznie alokowaną macierz
 
     void allocateMemory(int size);
+public:
+    // Konstruktory i destruktor
+    matrix();
+    matrix(int n);
+    matrix(int n, int* t);
+    matrix(const matrix& m);
+    ~matrix();
+
+    // Metody
+    matrix& alokuj(int n);
+    matrix& wstaw(int x, int y, int wartosc);
+    int pokaz(int x, int y) const;
+    matrix& dowroc();
+    matrix& losuj();
+    matrix& losuj(int x);
+    matrix& diagonalna(int* t);
+    matrix& diagonalna_k(int k, int* t);
+    matrix& kolumna(int x, int* t);
+    matrix& wiersz(int y, int* t);
+    matrix& przekatna();
+    matrix& pod_przekatna();
+    matrix& nad_przekatna();
+    matrix& szachownica();
