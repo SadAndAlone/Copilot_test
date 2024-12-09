8f9379f6ba26f2654eea4c62801ceadf35c1c042
TrudovMykhailo <156135082+SadAndAlone@users.noreply.github.com>
2024-12-09 14:24:03 +0100

    matrix.cpp dodny

---
+++ Copilot_trying/matrix.cpp
@@ -0,0 +1,9 @@
+﻿#include "matrix.h"
+
+// --- Pomocnicza funkcja alokująca pamięć ---
+void matrix::allocateMemory(int size) {
+    data = new int* [size];
+    for (int i = 0; i < size; ++i) {
+        data[i] = new int[size]();
+    }
+}
